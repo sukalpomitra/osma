44c01e501ce2406213776cbd3b59c48d5e02cab1
Karthik THIRUGNANA SURIYAN <ktsuriyan@palo-it.com>
2019-07-30 15:20:09 +0800

    added overfloe menu frame for credential page

--- src/Osma.Mobile.App/Views/Credentials/CredentialPage.xaml.cs
+++ src/Osma.Mobile.App/Views/Credentials/CredentialPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Osma.Mobile.App.Views.Credentials
@@ -8,7 +9,13 @@ namespace Osma.Mobile.App.Views.Credentials
     {
         public CredentialPage()
         {
+            NavigationPage.SetHasNavigationBar(this, false);
             InitializeComponent();
+        }
+
+        private void ToggleModalTapped(object sender, EventArgs e)
+        {
+            moreModal.IsVisible = !moreModal.IsVisible;
         }
     }
 }
