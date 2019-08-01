c9987a25f1a23c0a7126d66c438b619314c6941c
gargamul <agarg@palo-it.com>
2019-08-01 11:59:07 +0800

    Able to pass fullName from FullNamePage to EmailAddressPage

--- src/Osma.Mobile.App/ViewModels/UserRegistration/EmailAddressViewModel.cs
+++ src/Osma.Mobile.App/ViewModels/UserRegistration/EmailAddressViewModel.cs
@@ -5,6 +5,7 @@ using AgentFramework.Core.Models.Wallets;
 using Osma.Mobile.App.Services.Interfaces;
 using Osma.Mobile.App.Services.Models;
 using Osma.Mobile.App.Views.UserRegistration;
+using ReactiveUI;
 using Xamarin.Forms;
 
 namespace Osma.Mobile.App.ViewModels
@@ -14,20 +15,41 @@ namespace Osma.Mobile.App.ViewModels
         private readonly ICustomAgentContextProvider _agentContextProvider;
         private readonly INavigationService _navigationService;
 
-        public EmailAddressViewModel(IUserDialogs userDialogs, 
+        public EmailAddressViewModel(IUserDialogs userDialogs,
                                  INavigationService navigationService,
-                                 ICustomAgentContextProvider agentContextProvider) : base(
-                                 nameof(EmailAddressViewModel), 
-                                 userDialogs, 
+                                 ICustomAgentContextProvider agentContextProvider,
+                                 string fullName
+                                 ) : base(
+                                 nameof(EmailAddressViewModel),
+                                 userDialogs,
                                  navigationService)
         {
             _navigationService = navigationService;
             _agentContextProvider = agentContextProvider;
+            FullName = fullName;
         }
 
         #region Bindable Command
         public ICommand OpenPhoneNumberPageCommand => new Command(async () => await _navigationService.NavigateToAsync<PhoneNumberViewModel>());
 
+        #endregion
+
+        #region Bindable Properties
+        private string _emailAddress;
+
+        public string EmailAddress
+        {
+            get => _emailAddress;
+            set => this.RaiseAndSetIfChanged(ref _emailAddress, value);
+        }
+
+        private string _fullName;
+
+        public string FullName
+        {
+            get => _fullName;
+            set => this.RaiseAndSetIfChanged(ref _fullName, value);
+        }
         #endregion
     }
 }
